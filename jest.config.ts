a1f3b4f1ad537dafc0cf08540efa08cb1c25693f
hatakiii <25lp5598@nest.edu.mn>
2026-02-11 22:49:00 +0800

    pinecone monorepo

--- jest.config.ts
+++ jest.config.ts
@@ -1,6 +1,5 @@
-import type { Config } from 'jest';
-import { getJestProjectsAsync } from '@nx/jest';
-
-export default async (): Promise<Config> => ({
-  projects: await getJestProjectsAsync(),
-});
+import { getJestProjects } from '@nx/jest';
+ 
+export default {
+  projects: getJestProjects(),
+};
---
+++ libs/shadcn/src/index.ts
@@ -0,0 +1,2 @@
+export * from './lib/shadcn';
+export * from './lib/utils';
